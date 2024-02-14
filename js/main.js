8da5f551c0c22765ef6297b62e816d3db4856b68
sungwookgimozzzi <xolidwook@gmail.com>
2024-02-14 21:00:13 +0900

    js추가
    js추가

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+
+
+
+
+
+
+
+
+
+
+
+
 const openJoinMod = document.querySelector('.joinus');
 const joinModal = document.querySelector('.modal');
 const closeJoinMod = document.querySelector('.close-modal');
@@ -10,12 +22,60 @@ function closeJoinModal(){
     joinModal.classList.add('hidden');
 }
 
-/*const pwdInput = document.querySelector('.user_pw');
-const showPwd = document.querySelector('.eye');
-
+const pwdInput = document.getElementById('user_pw');
+const showPwd = document.getElementById('openeye');
 showPwd.addEventListener('click',function(){
     if(pwdInput.type === 'password') {
         pwdInput.type = 'text';
-        showPwd
+        showPwd.src = './img/eye.svg';
+    }else{
+        pwdInput.type = 'password';
+        showPwd.src = './img/eye-slash.svg';
     }
-})*/
+});
+
+
+
+const pwdCheck = document.getElementById('user_pw_check');
+const showPwdCheck = document.getElementById('openeye2');
+showPwdCheck.addEventListener('click',function(){
+    if(pwdCheck.type === 'password') {
+        pwdCheck.type = 'text';
+        showPwdCheck.src = './img/eye.svg';
+    }else{
+        pwdCheck.type = 'password';
+        showPwdCheck.src = './img/eye-slash.svg';
+    }
+});
+
+/*
+function checkKor(event) {
+    // const regExp = /[^0-9a-zA-Z]/g; // 숫자와 영문자만 허용
+    const regExp = /[^ㄱ-ㅎ|가-힣]/g; // 한글만 허용
+    const del = event.target;
+    if (regExp.test(del.value)) {
+      del.value = del.value.replace(regExp, '');
+    }
+  };
+*/
+
+/*
+function checkKor(event) {
+    // const regExp = /[^0-9a-zA-Z]/g; // 숫자와 영문자만 허용
+    const checkKor = /[^ㄱ-ㅎ|가-힣]/g; // 한글만 허용
+    const del = event.target;
+    if (checkKor.test(del.value)) {
+      del.value = del.value.replace(checkKor, '');
+    }
+  };
+*/
+function checkKor(k){
+    k.value  = k.value.replace(/[^ㄱ-힣]+/ig, '');
+}
+
+function checkEngNum(e1){
+    e1.value  = e1.value.replace(/[^A-Za-z0-9]+/ig, '');
+}
+
+
+
