5a4be07548a6644effb864317c75c3918890e2e4
DengChanKim <tigeryck0812@gmail.com>
2024-02-22 03:03:57 +0900

    [회원가입 모달] 비밀번호 및 비밀번호 확인 불일치 시 알럿 및 문구 추가

--- js/main.js
+++ js/main.js
@@ -23,6 +23,7 @@ function showJoinModal(){
 }
 function closeJoinModal(){
     joinModal.classList.add('hidden');
+    mismatchNoticeActive(false);
 }
 
 // 비밀번호 마스킹 버튼 이벤트 추가
@@ -79,4 +80,38 @@ function checkKor(k){
 
 function checkEngNum(e1){
     e1.value  = e1.value.replace(/[^A-Za-z0-9]+/ig, '');
+}
+
+// 비밀번호 일치 여부 확인
+function register(event) {
+    event.preventDefault();
+    var form = document.getElementById("joinus-modal-form");
+    var formData = new FormData(form);
+    var userName = formData.get("user-name");
+    var userId = formData.get("user-id");
+    var userPw = formData.get("user-pw");
+    var userPwCheck = formData.get("user-pw-check");
+
+
+    if (userPw !== userPwCheck){
+        // 알럿 노출
+        alert("비밀번호가 일치하지 않습니다.");
+        // 비밀번호 불일치 안내문구 노출
+        mismatchNoticeActive(true);
+        return;
+    };
+    console.log("pass");
+}
+
+function mismatchNoticeActive(active) {
+    var mismatchNotice = document.querySelector(".joinus-modal .user-pw-check .mismatch-notice");
+    var registerBtn = document.querySelector(".joinus-modal .register-btn");
+    if (active) {
+        mismatchNotice.classList.remove("hidden");
+        registerBtn.classList.add("mismatch");
+    }
+    else {
+        mismatchNotice.classList.add("hidden");
+        registerBtn.classList.remove("mismatch");
+    };
 }
