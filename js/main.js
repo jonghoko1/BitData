07d75ea8a0cb14f2d0d676d5fb0a839a9df7b54b
DengChanKim <tigeryck0812@gmail.com>
2024-02-23 01:55:32 +0900

    닫기 버튼 드래그 방지 및 조건 충족 시 최종 버튼 활성화

--- js/main.js
+++ js/main.js
@@ -7,12 +7,15 @@ function inputTagInit(inputTag) {
 const openLoginMod = document.querySelector('.login');
 const loginModal = document.querySelector('.login-modal');
 const closeLoginMod = document.querySelector('.close-login-modal');
-openLoginMod.addEventListener('click',showLoginModal);
-closeLoginMod.addEventListener('click',closeLoginModal);
-function showLoginModal(){
+closeLoginMod.ondragstart = function () {
+    return false;
+}
+openLoginMod.addEventListener('click', showLoginModal);
+closeLoginMod.addEventListener('click', closeLoginModal);
+function showLoginModal() {
     loginModal.classList.remove('hidden');
 }
-function closeLoginModal(){
+function closeLoginModal() {
     loginModal.classList.add('hidden');
     inputTagInit(document.querySelectorAll('.login-modal input')[0]);
     inputTagInit(document.querySelectorAll('.login-modal input')[1]);
@@ -21,13 +24,16 @@ function closeLoginModal(){
 // JoinUs Modal PopUp 닫기 버튼
 const openJoinMod = document.querySelector('.joinus');
 const joinModal = document.querySelector('.joinus-modal');
-const closeJoinMod = document.querySelector('.close-modal');
-openJoinMod.addEventListener('click',showJoinModal);
-closeJoinMod.addEventListener('click',closeJoinModal);
-function showJoinModal(){
+const closeJoinMod = document.querySelector('.close-joinus-modal');
+closeJoinMod.ondragstart = function () {
+    return false;
+}
+openJoinMod.addEventListener('click', showJoinModal);
+closeJoinMod.addEventListener('click', closeJoinModal);
+function showJoinModal() {
     joinModal.classList.remove('hidden');
 }
-function closeJoinModal(){
+function closeJoinModal() {
     joinModal.classList.add('hidden');
     inputTagInit(document.querySelectorAll('.joinus-modal input')[0]);
     inputTagInit(document.querySelectorAll('.joinus-modal input')[1]);
@@ -37,23 +43,91 @@ function closeJoinModal(){
     checkForDuplication(false);
 }
 
+const loginUserId = document.getElementById('login-user-id'); // 로그인 아이디 입력칸
+const loginUserPw = document.getElementById('login-user-pw'); // 로그인 비밀번호 입력칸
+const loginBtn = document.querySelector('.login-modal .login-btn button'); // 로그인 버튼
+
+const joinusUserName = document.getElementById('joinus-user-name'); // 회원가입 이름 입력칸
+const joinusUserId = document.getElementById('joinus-user-id'); // 회원가입 아이디 입력칸
+const joinusUserPw = document.getElementById('joinus-user-pw'); // 회원가입 비밀번호 입력칸
+const joinusUserPwCheck = document.getElementById('joinus-user-pw-check'); // 회원가입 비밀번호 확인 입력칸
+const registerBtn = document.querySelector('.joinus-modal .register-btn button'); // 회원가입 버튼
+
+// 로그인 모달 입력칸 이벤트 등록
+loginUserId.addEventListener('input', function () {
+    checkEngNum(loginUserId);
+    loginBtnActive();
+});
+loginUserPw.addEventListener('input', function () {
+    checkEngNum(loginUserPw);
+    loginBtnActive();
+});
+
+// 회원가입 모달 입력칸 이벤트 등록
+joinusUserName.addEventListener('input', function () {
+    checkKor(joinusUserName);
+    joinusBtnActive();
+});
+joinusUserId.addEventListener('input', function () {
+    checkEngNum(joinusUserId);
+    joinusBtnActive();
+});
+joinusUserPw.addEventListener('input', function () {
+    checkEngNum(joinusUserPw);
+    joinusBtnActive();
+});
+joinusUserPwCheck.addEventListener('input', function () {
+    checkEngNum(joinusUserPwCheck);
+    joinusBtnActive();
+});
+
 // 이름 입력 양식 (한글만 허용)
-function checkKor(input){
+function checkKor(input) {
     input.value = input.value.replace(/[^ㄱ-힣]+/ig, '');
 }
 // 아이디 및 비밀번호 입력 양식 (영문/숫자만 허용)
-function checkEngNum(input){
-    input.value  = input.value.replace(/[^A-Za-z0-9]+/ig, '');
+function checkEngNum(input) {
+    input.value = input.value.replace(/[^A-Za-z0-9]+/ig, '');
 }
 
-// 회원가입 아이디 입력칸
-const joinusUserId = document.getElementById('joinus-user-id');
+// 로그인 버튼 활성화 비활성화
+function loginBtnActive() {
+    if (loginUserId.value.length > 4 &&
+        loginUserPw.value.length > 7
+    ) {
+        loginBtn.classList.replace('inactive-btn', 'active-btn');
+        loginBtn.disabled = false;
+    }
+    else {
+        loginBtn.classList.replace('active-btn', 'inactive-btn');
+        loginBtn.disabled = true;
+    };
+}
+
+// 회원가입 버튼 활성화 비활성
+function joinusBtnActive() {
+    if (joinusUserName.value.length > 1 &&
+        joinusUserId.classList.contains('confirmed-input') &&
+        joinusUserPw.value.length > 7 &&
+        joinusUserPwCheck.value.length > 7
+
+    ) {
+        registerBtn.classList.replace('inactive-btn', 'active-btn');
+        registerBtn.disabled = false;
+    }
+    else {
+        registerBtn.classList.replace('active-btn', 'inactive-btn');
+        registerBtn.disabled = true;
+    };
+}
+
 // 회원가입 아이디 중복확인 버튼
 const checkForDuplicationBtn = document.querySelector('.check-for-duplication-btn');
 
-joinusUserId.addEventListener('input', function() {
+joinusUserId.addEventListener('input', function () {
     checkForDuplicationBtnSet();
 });
+
 // 회원가입 아이디 중복확인 버튼 상태 변경
 function checkForDuplicationBtnSet() {
     if (joinusUserId.value.length > 4) {
@@ -67,7 +141,7 @@ function checkForDuplicationBtnSet() {
 }
 
 // 회원가입 아이디 중복확인
-checkForDuplicationBtn.addEventListener('click', function() {
+checkForDuplicationBtn.addEventListener('click', function () {
     checkForDuplication(true);
 });
 function checkForDuplication(result) {
@@ -75,7 +149,7 @@ function checkForDuplication(result) {
         checkForDuplicationBtn.classList.replace('active-btn', 'confirmed-btn');
         checkForDuplicationBtn.disabled = true;
         checkForDuplicationBtn.textContent = '확인완료';
-        
+
         joinusUserId.classList.replace('unconfirmed-input', 'confirmed-input');
         joinusUserId.disabled = true;
     }
@@ -83,36 +157,36 @@ function checkForDuplication(result) {
         checkForDuplicationBtn.classList.replace('confirmed-btn', 'inactive-btn');
         checkForDuplicationBtn.disabled = true;
         checkForDuplicationBtn.textContent = '중복확인';
-        
+
         joinusUserId.classList.replace('confirmed-input', 'unconfirmed-input');
         joinusUserId.disabled = false;
     };
+    joinusBtnActive();
 }
 
 // 로그인 모달 비밀번호 버튼 마스킹 기능 추가
-passwordShowBtnDown('login-user-pw', 'openeye3');
+passwordShowBtnDown(loginUserPw, 'openeye3');
 // 회원가입 모달 비밀번호 버튼 마스킹 기능 추가
-passwordShowBtnDown('joinus-user-pw', 'openeye1');
+passwordShowBtnDown(joinusUserPw, 'openeye1');
 // 회원가입 모달 비밀번호 확인 버튼 마스킹 기능 추가
-passwordShowBtnDown('joinus-user-pw-check', 'openeye2');
+passwordShowBtnDown(joinusUserPwCheck, 'openeye2');
 
 // 비밀번호 마스킹 버튼 이벤트 추가
-function passwordShowBtnDown(pwdInputId, btnId) {
-    pwdInputId = document.getElementById(pwdInputId);
+function passwordShowBtnDown(input, btnId) {
     btnId = document.getElementById(btnId);
     // 버튼 드래그 방지
-    btnId.ondragstart = function() {
+    btnId.ondragstart = function () {
         return false;
     };
 
-    btnId.addEventListener('mousedown', function() {
-        pwdInputId.type = 'text';
+    btnId.addEventListener('mousedown', function () {
+        input.type = 'text';
         btnId.querySelector('img').src = './img/eye.svg';
-        document.addEventListener('mouseup', function() {
-            pwdInputId.type = 'password';
+        document.addEventListener('mouseup', function () {
+            input.type = 'password';
             btnId.querySelector('img').src = './img/eye-slash.svg';
         },
-        {once: true});
+            { once: true });
     });
 }
 
@@ -126,8 +200,7 @@ function register(event) {
     var userPw = formData.get('joinus-user-pw');
     var userPwCheck = formData.get('joinus-user-pw-check');
 
-
-    if (userPw !== userPwCheck){
+    if (userPw !== userPwCheck) {
         // 알럿 노출
         alert("비밀번호가 일치하지 않습니다.");
         // 비밀번호 불일치 안내문구 노출
