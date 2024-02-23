731a4f89663e00418681832457e29984c947878d
RCN_98th_no21 <jonghoko3@naver.com>
2024-02-23 22:47:31 +0900

    회원가입 3번, 로그인 2번
    회원가입 3번
    회원가입 버튼 클릭 및 성공 시 성공 알럿창 띄운 후 로그인 모달로 전환
    
    로그인 2번
    회원가입하기 버튼 클릭 시 회원가입 모달로 전환

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-// 인풋박스 입력값 초기화
-function inputTagInit(inputTag) {
-    inputTag.value = null;
-}
 
 // Login Modal PopUp 닫기 버튼
 const openLoginMod = document.querySelector('.login');
@@ -37,10 +33,16 @@ function closeJoinModal(){
     checkForDuplication(false);
 }
 
+// 모달 닫기 시 인풋박스 입력값 초기화
+function inputTagInit(inputTag) {
+    inputTag.value = null;
+}
+
 // 이름 입력 양식 (한글만 허용)
 function checkKor(input){
     input.value = input.value.replace(/[^ㄱ-힣]+/ig, '');
 }
+
 // 아이디 및 비밀번호 입력 양식 (영문/숫자만 허용)
 function checkEngNum(input){
     input.value  = input.value.replace(/[^A-Za-z0-9]+/ig, '');
@@ -48,12 +50,14 @@ function checkEngNum(input){
 
 // 회원가입 아이디 입력칸
 const joinusUserId = document.getElementById('joinus-user-id');
+
 // 회원가입 아이디 중복확인 버튼
 const checkForDuplicationBtn = document.querySelector('.check-for-duplication-btn');
 
 joinusUserId.addEventListener('input', function() {
     checkForDuplicationBtnSet();
 });
+
 // 회원가입 아이디 중복확인 버튼 상태 변경
 function checkForDuplicationBtnSet() {
     if (joinusUserId.value.length > 4) {
@@ -116,6 +120,16 @@ function passwordShowBtnDown(pwdInputId, btnId) {
     });
 }
 
+// 로그인 모달에서 회원가입하기 버튼 클릭 시 회원가입 모달로 전환
+const switchToJoinusBtn = document.querySelector('.switch-to-joinus');
+switchToJoinusBtn.addEventListener('click',switchToJoinus);
+function switchToJoinus(){
+    loginModal.classList.add("hidden");
+    joinModal.classList.remove("hidden");
+}
+
+// 회원가입 모달의 회원가입 버튼 클릭 시
+
 // 비밀번호 일치 여부 확인
 function register(event) {
     event.preventDefault();
@@ -128,12 +142,35 @@ function register(event) {
 
 
     if (userPw !== userPwCheck){
+        // 비밀번호 불일치 시
         // 알럿 노출
         alert("비밀번호가 일치하지 않습니다.");
         // 비밀번호 불일치 안내문구 노출
         mismatchNoticeActive(true);
         return;
+    }
+    else {
+        // 비밀번호 일치 시(회원가입 요청 API 호출)
+        // 현재는 바로 성공 함수 호출(API 개발 되면 API 함수 호출로 변경 필요)
+        registerSuccess();
     };
+}
+
+// 회원가입 요청 API 호출
+// const registerRequestBtn = document.querySelector('.register-requests');
+// registerRequestBtn.addEventListener('click',registerRequest);
+// function registerRequest(){
+//     ~
+// }
+
+// 회원가입 요청 API 호출 response 로 true 값 반환 된 경우
+// API response 값을 조건으로 잡아야함(현재는 API가 없으니 비밀번호가 일치하면 실행하도록 진행 > 추후 변경 필요)
+function registerSuccess(){
+    // 회원가입 성공 알럿창 노출
+    alert("회원가입이 완료되었습니다.");
+    // 로그인 모달로 전환
+    joinModal.classList.add("hidden");
+    loginModal.classList.remove("hidden");
 }
 
 // 비밀번호 불일치 안내문 활성화 및 비활성화 기능
@@ -148,12 +185,4 @@ function mismatchNoticeActive(active) {
         mismatchNotice.classList.add('hidden');
         registerBtn.classList.remove('mismatch');
     };
-}
-
-// 회원가입하기 버튼 클릭 시 모달 전환
-const asdf = document.querySelector('.switch-to-joinus')
-asdf.addEventListener('click',switchToJoinus);
-function switchToJoinus(){
-    loginModal.classList.add("hidden");
-    joinModal.classList.remove("hidden");
 }
