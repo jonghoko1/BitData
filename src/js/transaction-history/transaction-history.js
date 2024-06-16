bdcd249c643055097a27c67bb830a5c19944dd5a
hurryduck <tigeryck0812@gmail.com>
2024-06-16 17:08:29 +0900

    feat: [업로드] 버튼 #22
    1. 이미지 메모장 모달 html 구조 변경
    2. 업로드 버튼 클릭시 파일 업로더 노출

--- src/js/transaction-history/transaction-history.js
+++ src/js/transaction-history/transaction-history.js
@@ -3,53 +3,68 @@
 const $imageMemoBtn = document.querySelector('.image-memo-btn'); // 거래내역 메모 버튼
 // 이미지 메모장 모달
 const $imageMemoModal = document.querySelector('.image-memo'); // 이미지 메모장 모달
-const $imageMemoModalBody = document.querySelector('.image-memo .modal-body'); // 모달 바디
-const $imageMemoModalFooter = document.querySelector('.image-memo .modal-footer'); // 모달 푸터
+const $imageMemoModalImageCardContainer = document.querySelector('.image-memo .image-card-container'); // 이미지 카드 컨테이너
 const $imageMemoModalCloseBtn = document.querySelector('.image-memo .close-btn'); // 닫기 버튼
-const $imageMemoModalViewBtn = document.querySelectorAll('.image-memo .view-btn'); // 보기 버튼
-const $imageMemoModalDeleteBtn = document.querySelectorAll('.image-memo .delete-btn'); // 삭제 버튼
+const $imageMemoModalViewBtns = document.querySelectorAll('.image-memo .view-btn'); // 보기 버튼들
+const $imageMemoModalDeleteBtns = document.querySelectorAll('.image-memo .delete-btn'); // 삭제 버튼들
+const $imageMemoModalImageUpload = document.getElementById('image-upload'); // 이미지 업로드 input
 const $imageMemoModalUploadBtn = document.querySelector('.image-memo .upload-btn'); // 업로드 버튼
 // 이미지 크게 보기 모달
 const $imageMemoViewModal = document.querySelector('.image-memo-view'); // 이미지 크게 보기 모달
 const $imageMemoViewModalCloseBtn = document.querySelector('.image-memo-view .close-btn'); // 닫기 버튼
 
-// method
+// methods
 // 거래내역
-const imageMemoBtnClick = function() { // 메모 버튼 클릭시
+const openImageMemoModal = function() { // 메모 버튼 클릭시
     $imageMemoModal.classList.remove('hidden'); // 모달 노출
     document.body.style.overflow = 'hidden'; // 뒷 배경 스크롤 방지
 };
 // 이미지 메모장 모달
-const imageMemoModalCloseBtnClick = function() { // 닫기 버튼 클릭시
-    $imageMemoModal.classList.add('hidden'); // 모달 노출
-    document.body.style.overflow = 'auto'; // 뒷 배경 스크롤 방지
+const closeImageMemoModal = function() { // 닫기 버튼 클릭시
+    $imageMemoModal.classList.add('hidden'); // 모달 숨기기
+    document.body.style.overflow = 'auto'; // 뒷 배경 스크롤 가능
 }
-const imageMemoModalViewBtnClick = function() { // 보기 버튼 클릭시
-    $imageMemoViewModal.classList.remove('hidden');
+const viewImageInModal = function() { // 보기 버튼 클릭시
+    $imageMemoViewModal.classList.remove('hidden'); // 이미지 크게 보기 모달 노출
 }
-const imageMemoModalDeleteBtnClick = function(event) { // [삭제] 버튼 클릭 시
-
+const deleteImageFromModal = function(event) { // 삭제 버튼 클릭시
     if (confirm('메모를 삭제하시겠습니까?')) {
         const $imageCard = event.target.closest('.image-card');
         $imageCard.remove();
         $imageMemoModalUploadBtn.parentElement.classList.remove('hidden'); // 업로드 버튼 노출
     }
 }
+const triggerImageUpload = function() { // 업로드 버튼 클릭시
+    $imageMemoModalImageUpload.click();
+}
+const handleImageUploadChange = function() { // 파일을 업로드했을때
+    const file = $imageMemoModalImageUpload.files[0];
+    if (file) { // file이 null이 아닐 때
+        uploadImage(file);
+    }
+}
+const uploadImage = function(file) {
+
+}
 // 이미지 크게 보기 모달
-const imageMemoModalViewBtnCloseBtnClick = function() { // 보기 버튼 클릭시
-    $imageMemoViewModal.classList.add('hidden');
+const closeImageViewModal = function() { // 닫기 버튼 클릭시
+    $imageMemoViewModal.classList.add('hidden'); // 모달 숨기기
 }
 
 // main
 // 거래내역
-$imageMemoBtn.addEventListener('click', imageMemoBtnClick); // 메모 버튼 클릭 이벤트 추가
+$imageMemoBtn.addEventListener('click', openImageMemoModal); // 메모 버튼 클릭 이벤트 추가
 // 이미지 메모장 모달
-$imageMemoModalCloseBtn.addEventListener('click', imageMemoModalCloseBtnClick) // 닫기 버튼 클릭 이벤트 추가
-for (let viewBtn of $imageMemoModalViewBtn) { // 보기 버튼 클릭 이벤트 추가
-    viewBtn.addEventListener('click', imageMemoModalViewBtnClick); 
-}
-for (let deleteBtn of $imageMemoModalDeleteBtn) { // 보기 버튼 클릭 이벤트 추가
-    deleteBtn.addEventListener('click', imageMemoModalDeleteBtnClick); 
-}
-// 이미지 크게 보기 모달
-$imageMemoViewModalCloseBtn.addEventListener('click', imageMemoModalViewBtnCloseBtnClick); // 닫기 버튼 클릭 이벤트 추가
+$imageMemoModalCloseBtn.addEventListener('click', closeImageMemoModal) // 닫기 버튼 클릭 이벤트 추가
+$imageMemoModalUploadBtn.addEventListener('click', triggerImageUpload); // 업로드 버튼 클릭 이벤트 추가
+$imageMemoModalImageUpload.addEventListener('change', handleImageUploadChange); // 이미지 업로드 변경 이벤트 추가
+// 보기 버튼 클릭 이벤트 추가
+$imageMemoModalViewBtns.forEach(viewBtn => {
+    viewBtn.addEventListener('click', viewImageInModal);
+});
+// 삭제 버튼 클릭 이벤트 추가
+$imageMemoModalDeleteBtns.forEach(deleteBtn => {
+    deleteBtn.addEventListener('click', deleteImageFromModal);
+});
+// 이미지 크게 보기 모달 닫기 버튼 클릭 이벤트 추가
+$imageMemoViewModalCloseBtn.addEventListener('click', closeImageViewModal);
