91764c9c5c3ef21c9461be41935448b8440911bf
hurryduck <tigeryck0812@gmail.com>
2024-06-16 18:27:11 +0900

    update: 업로드 버튼 기능 추가 #22
    1. 파일 업로드 및 이미지 카드 추가
    2. 크기 유효성 검사

--- src/js/transaction-history/transaction-history.js
+++ src/js/transaction-history/transaction-history.js
@@ -8,6 +8,7 @@ const $imageMemoModalCloseBtn = document.querySelector('.image-memo .close-btn')
 const $imageMemoModalViewBtns = document.querySelectorAll('.image-memo .view-btn'); // 보기 버튼들
 const $imageMemoModalDeleteBtns = document.querySelectorAll('.image-memo .delete-btn'); // 삭제 버튼들
 const $imageMemoModalImageUpload = document.getElementById('image-upload'); // 이미지 업로드 input
+const MAXSIZE = 1024 * 1024; // 1MB
 const $imageMemoModalUploadBtn = document.querySelector('.image-memo .upload-btn'); // 업로드 버튼
 // 이미지 크게 보기 모달
 const $imageMemoViewModal = document.querySelector('.image-memo-view'); // 이미지 크게 보기 모달
@@ -26,52 +27,99 @@ const openImageMemoModal = function() { // 이미지 메모장 모달 열기
 const closeImageMemoModal = function() { // 이미지 메모장 모달 닫기
     $imageMemoModal.classList.add('hidden'); // 모달 숨기기
     document.body.style.overflow = 'auto'; // 뒷 배경 스크롤 가능
-}
+};
 const viewImageInModal = function() { // 보기 버튼 클릭시
     $imageMemoViewModal.classList.remove('hidden'); // 이미지 크게 보기 모달 노출
-}
-const deleteImageFromModal = function(event) { // 삭제 버튼 클릭시
+};
+const deleteImageCard = function(event) { // 삭제 버튼 클릭시
     if (confirm('메모를 삭제하시겠습니까?')) {
         const $imageCard = event.target.closest('.image-card');
         $imageCard.remove();
         $imageMemoModalUploadBtn.parentElement.classList.remove('hidden'); // 업로드 버튼 노출
     }
-}
-const triggerImageUpload = function() { // 파일 업로더 노출
+};
+const clickImageUpload = function() { // 파일 업로더 노출
     $imageMemoModalImageUpload.click();
-}
+};
 const handleImageUploadChange = function() { // 파일을 업로드했을때
     const file = $imageMemoModalImageUpload.files[0];
     if (file) { // file이 null이 아닐 때
-        uploadImage(file);
+        if (isValidFileSize(file)) {
+            uploadImage(file);
+        } else {
+            alert(`
+            이미지 업로드 제한 사항을 확인해주세요.
+            용량 제한: 1MB 이하
+            `);
+        }
+        $imageMemoModalImageUpload.value = '';
     }
-}
-const uploadImage = function(file) {
+};
+const isValidFileSize = (file) => { // 파일크기 유효성 검사
+    return file.size <= MAXSIZE;
+};
+const uploadImage = function(image) {
+    const reader = new FileReader();
+    reader.onload = (image) => {
+        const newImageCard = document.createElement('div');
+        const title = 'IMAGE0001';
+        const date = formatDate(new Date());
+        newImageCard.classList.add('image-card');
+        newImageCard.innerHTML = `
+            <img src="${image.target.result}" alt="Image">
+            <div class="info">
+                <div class="title">${title}</div>
+                <div class="description">
+                    디스크립션
+                </div>
+                <div class="upload-date">${date}</div>
+            </div>
+            <div class="actions">
+                <button class="view-btn">보기</button>
+                <button class="delete-btn">삭제</button>
+            </div>
+        `;
+        $imageMemoModalImageCardContainer.appendChild(newImageCard);
 
-}
-// 이미지 크게 보기 모달
+        newImageCard.querySelector('.view-btn').addEventListener('click', viewImageInModal); // 새로운 [보기] 버튼에 이벤트 리스너 추가
+        newImageCard.querySelector('.delete-btn').addEventListener('click', deleteImageCard); // 새로운 [삭제] 버튼에 이벤트 리스너 추가 
+
+        if ($imageMemoModal.querySelectorAll('.image-card').length === 3) { // 이미지 카드가 3개일 경우 업로드 버튼 숨김
+            $imageMemoModalUploadBtn.parentElement.classList.add('hidden'); // 업로드 버튼 숨김
+        }
+    };
+    reader.readAsDataURL(image);
+};
+const formatDate = function(date) { // 날짜 형식
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
+};
 const closeImageViewModal = function() { // 이미지 크게 보기 모달 닫기
     $imageMemoViewModal.classList.add('hidden');
-}
+};
 // 메모 수정 모달
 const openMemoUpdateModal = function() { // 메모 수정 모달 열기
     $memoUpdateModal.classList.remove('hidden');
-}
+};
 
 // main
 // 거래내역
-$imageMemoBtn.addEventListener('click', openImageMemoModal); // 메모 버튼 클릭 이벤트 추가
+$imageMemoBtn.addEventListener('click', openImageMemoModal); // [메모] 버튼 클릭 이벤트 추가
 // 이미지 메모장 모달
-$imageMemoModalCloseBtn.addEventListener('click', closeImageMemoModal) // 닫기 버튼 클릭 이벤트 추가
-$imageMemoModalUploadBtn.addEventListener('click', triggerImageUpload); // 업로드 버튼 클릭 이벤트 추가
+$imageMemoModalCloseBtn.addEventListener('click', closeImageMemoModal) // [닫기] 버튼 클릭 이벤트 추가
+$imageMemoModalUploadBtn.addEventListener('click', clickImageUpload); // [업로드] 버튼 클릭 이벤트 추가
 $imageMemoModalImageUpload.addEventListener('change', handleImageUploadChange); // 이미지 업로드 변경 이벤트 추가
 // 보기 버튼 클릭 이벤트 추가
 $imageMemoModalViewBtns.forEach(viewBtn => {
-    viewBtn.addEventListener('click', viewImageInModal);
+    viewBtn.addEventListener('click', viewImageInModal); // 이미지 카드 [보기] 버튼 이벤트 등록
 });
 // 삭제 버튼 클릭 이벤트 추가
 $imageMemoModalDeleteBtns.forEach(deleteBtn => {
-    deleteBtn.addEventListener('click', deleteImageFromModal);
+    deleteBtn.addEventListener('click', deleteImageCard); // 이미지 카드 [삭제] 버튼 이벤트 등록
 });
 // 이미지 크게 보기 모달 닫기 버튼 클릭 이벤트 추가
 $imageMemoViewModalCloseBtn.addEventListener('click', closeImageViewModal);
