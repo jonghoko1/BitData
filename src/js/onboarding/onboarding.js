d2ff142a2626ca3364d20cac73396443aa5832b6
hurryduck <tigeryck0812@gmail.com>
2024-06-29 21:02:51 +0900

    feat: 입력란 및 [시작하기] 버튼 기능 추가 #28
    1. 입력란
      * 영문, 숫자만 입력 가능하게
      * 값 없을 경우 비활성화
    2. 시작 버튼 클릭시 데이터 수집화면으로 이동

---
+++ src/js/onboarding/onboarding.js
@@ -0,0 +1,55 @@
+const $apiManagementFrm = document.querySelector('#api-management-frm'); // API Management 폼
+const $apiKeyInputField = document.querySelector('#api-key'); // api-key 입력란
+const $secretKeyInputField = document.querySelector('#secret-key'); // secret-key 입력란
+const $startBtn = document.querySelector('.start-btn'); // [시작하기] 버튼
+
+
+$apiKeyInputField.addEventListener('input', (event) => { // api-key 입력란 값 입력시
+    checkInputValue(event); // 영문과 숫자가 아닌 문자가 포함되어 있는지 검사 후 빈문자열로 대체
+    checkActive(); // api-key 입력란과 secret-key 입력란에 값이 있을 경우 활성화
+});
+
+$secretKeyInputField.addEventListener('input', (event) => { // secret-key 입력란 값 입력시
+    checkInputValue(event); // 영문과 숫자가 아닌 문자가 포함되어 있는지 검사 후 빈문자열로 대체
+    checkActive(); // api-key 입력란과 secret-key 입력란에 값이 있을 경우 활성화
+});
+
+// [시작하기] 버튼
+$startBtn.addEventListener('click', (event) => { // 활성화 상태가 아니라면 제출하지 않음
+    if (!$startBtn.classList.contains('active')) {
+        event.preventDefault();
+    }
+});
+
+// API Management 폼 이벤트 제출 이벤트
+$apiManagementFrm.addEventListener('submit', (event) => {
+    event.preventDefault(); // 기존 체출 동작 비활성화
+    spendApiKey();
+});
+
+
+function checkInputValue(event) { // 영문과 숫자가 아닌 문자가 포함되어 있는지 검사 후 빈문자열로 대체
+    const inputField = event.target;
+    const inputValue = inputField.value;
+    const pattern = /[^a-zA-Z0-9]/g; // 영문과 숫자가 아닌지 검사
+
+    if (pattern.test(inputValue)) { // 영문과 숫자가 아닌 문자가 포함되어 있는지 검사
+        inputField.value = inputValue.replace(pattern, ''); // 포함되어 있을 경우 빈칸으로 대체
+    }
+}
+
+function checkActive() { // api-key 입력란과 secret-key 입력란에 값이 있을 경우 활성화
+    console.log($apiKeyInputField.value);
+    console.log($secretKeyInputField.value);
+    if ($apiKeyInputField.value !== '' &&
+        $secretKeyInputField.value !== ''
+    ) {
+        $startBtn.classList.add('active');
+    } else {
+        $startBtn.classList.remove('active');
+    }
+}
+
+function spendApiKey() {
+    window.location.href = 'data-collction.html';
+}
