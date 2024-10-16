e17a56d35a6104634b30607bc2865cd1a0f966f1
hurryduck <tigeryck0812@gmail.com>
2024-10-16 15:32:30 +0900

    improvement: app.js 동적할당

--- app.js
+++ app.js
@@ -12,49 +12,10 @@ const __dirname = path.dirname(__filename);
 // 'dist' 폴더 내의 정적 파일들을 서빙
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// 경로에 따라 특정 HTML 파일 서빙
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
-app.get('/notice', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'notice.html'));
-});
-
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'src/pages/main/login/login.html'));
-});
-
-app.get('/join', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'src/pages/main/join/join.html'));
-});
-
-app.get('/onboarding', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'src/pages/main/onboarding/onboarding.html'));
-});
-
-app.get('/collect', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'src/pages/main/collect/collect.html'));
-});
-
-app.get('/dashboard', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'src/pages/service/dashboard/dashboard.html'));
-});
-
-app.get('/settings', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'src/pages/service/settings/settings.html'));
-});
-
-app.get('/faq', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'src/pages/service/faq/faq.html'));
-});
-
-app.get('/history', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'src/pages/service/history/history.html'));
-});
-
-app.get('/exit', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'src/pages/service/exit/exit.html'));
+// 경로에 따라 동적으로 HTML 파일 서빙
+app.get('/*', (req, res) => {
+  const fileName = req.params[0] ? req.params[0] : 'index'; // 기본 경로는 index.html로 설정
+  res.sendFile(path.join(__dirname, 'dist', `${fileName}.html`));
 });
 
 // 서버 실행
