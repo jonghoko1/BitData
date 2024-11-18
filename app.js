0868b9ca1ce167825099a4828dfda2f2128c8d81
hurryduck <tigeryck0812@gmail.com>
2024-11-18 23:18:26 +0900

    feat: add kakao login

--- app.js
+++ app.js
@@ -1,26 +1,101 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import path from "path";
+import axios from "axios";
+import { fileURLToPath } from "url";
 
 const app = express();
 
-// 환경 변수에서 포트를 읽거나 기본값으로 3000을 사용
 const PORT = process.env.PORT || 3000;
+const SERVER_URL = "http://127.0.0.1:8080/auths";
 
-// 현재 파일의 디렉토리 경로를 가져옵니다.
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// 'dist' 폴더 내의 정적 파일들을 서빙
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, "dist")));
 
-// 경로에 따라 동적으로 HTML 파일 서빙
-app.get('/*', (req, res) => {
-  const fileName = req.params[0] ? req.params[0] : 'index'; // 기본 경로는 index.html로 설정
-  res.sendFile(path.join(__dirname, 'dist', `${fileName}.html`));
+app.get("/login/kakao", async (req, res) => {
+    try {
+        const response = await axios.get(`${SERVER_URL}/login/kakao`);
+        res.redirect(response.data.kakao_auth_url);
+    } catch (error) {
+        res.status(500).send("Server Error");
+    }
 });
 
-// 서버 실행
+app.get("/login/kakao/auths/callback", async (req, res) => {
+    const request = req.query;
+    const code    = request.code;
+    const state   = request.state;
+
+    if (!code) {
+        return res.status(400).send("Authorization code is missing");
+    } else if (!state) {
+        return res.status(400).send("Authorization state is missing");
+    }
+
+    try {
+        const response = await getLoginKaKaoCallback(code, state);
+
+        if (!response) {
+            return res.status(500).send("Response is null");
+        }
+
+        return res.redirect(response.is_user ? "/history" : "/join");
+    } catch (error) {
+        res.status(500).send("Server Error: " + error);
+    }
+});
+
+async function getLoginKaKaoCallback(code, state) {
+    try {
+        const response = await axios.post(
+            `${SERVER_URL}/login/kakao/callback`,
+            new URLSearchParams({
+                code, 
+                state,
+            }),
+            {
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+            },
+        );
+
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+app.get("/join/kakao", async (req, res) => {
+    try {
+        const response = await getJoinKaKaoCallback();
+    
+        if (!response) {
+            return res.status(500).send("Response is null");
+        }
+
+        return res.redirect("/onboarding");
+    } catch (error) {
+        res.status(500).send("Server Error: " + error);
+    }
+});
+
+async function getJoinKaKaoCallback() {
+    try {
+        const response = await axios.get(`${SERVER_URL}/login/kakao/consent`)
+
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+app.get("/*", (req, res) => {
+    const fileName = req.params[0] ? req.params[0] : "index";
+    res.sendFile(path.join(__dirname, "dist", `${fileName}.html`));
+});
+
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
       {
-        name: "bitdata",        // 애플리케이션 이름
+        name: "bitdata",         // 애플리케이션 이름
         script: "./app.js",      // 실행할 스크립트 파일
         instances: 1,            // 인스턴스 개수 (0이면 클러스터 모드로 CPU 코어 수만큼 생성)
         exec_mode: "fork",       // 실행 모드 (fork 또는 cluster)
